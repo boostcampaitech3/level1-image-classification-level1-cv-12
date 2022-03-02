ededb8f3d4b7991dda70334f8b8e2dd17557a191
ptwd <zaq5526@gmail.com>
2022-03-02 13:12:23 +0900

    add Bertadam optimizer, CosineAnnealingWarmUpRestarts scheduler, mixup

--- train_f1_tracking_v1.py
+++ train_f1_tracking_v1.py
@@ -16,10 +16,14 @@ import torch
 import torch.nn as nn
 import torchvision.models as models
 from torch.optim.lr_scheduler import StepLR
+from torch.optim.lr_scheduler import OneCycleLR
 from torch.utils.data import DataLoader
 from torch.utils.tensorboard import SummaryWriter
 from efficientnet_pytorch import EfficientNet
 from sklearn.metrics import f1_score, classification_report
+
+from torchvision.transforms.functional import to_pil_image
+from albumentations.augmentations import Normalize
 
 import torchvision
 from torchvision import transforms
@@ -33,6 +37,9 @@ from loss import create_criterion
 
 from tqdm import tqdm
 from sklearn.metrics import f1_score, classification_report
+
+from transformers import AdamW as BERTAdam #optimizer
+
 
 
 # ========== Error analysis code ==========
@@ -69,11 +76,9 @@ def seed_everything(seed):
     np.random.seed(seed)
     random.seed(seed)
 
-
 def get_lr(optimizer):
     for param_group in optimizer.param_groups:
         return param_group['lr']
-
 
 def grid_image(np_images, gts, preds, n=16, shuffle=False):
     batch_size = np_images.shape[0]
@@ -120,7 +125,6 @@ def select_model(model, num_classes = 18):
         model_ = EfficientNet.from_pretrained('efficientnet-b0',num_classes = num_classes)
     return model_
 
-
 def increment_path(path, exist_ok=False):
     """ Automatically increment path, i.e. runs/exp --> runs/exp0, runs/exp1 etc.
 
@@ -137,27 +141,7 @@ def increment_path(path, exist_ok=False):
         i = [int(m.groups()[0]) for m in matches if m]
         n = max(i) + 1 if i else 2
         return f"{path}{n}"
-
-	# torchvision transform	
-# def get_transforms(need=('train', 'val'), img_size=(512, 384)):	
-#     transformations = {}	
-#     if 'train' in need:	
-#         transformations['train'] = torchvision.transforms.Compose([	
-#             Resize((224,224),Image.BILINEAR),	
-#             RandomRotation([-8, +8]),	
-#             ColorJitter(brightness=0.5, saturation=0.5, hue=0.5),  # todo : param	
-#             ToTensor(),	
-#             Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),	
-#         ])	
-#     if 'val' in need:	
-#         transformations['val'] = torchvision.transforms.Compose([	
-#             Resize((224,224),Image.BILINEAR),	
-#             ToTensor(),	
-#             Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),	
-#         ])	
-#     return transformations    	
-    	
-# albumentations    	
+ 	
 def get_transforms(need=('train', 'val'), img_size=(512, 384)):	
     transformations = {}	
     if 'train' in need:	
@@ -179,6 +163,78 @@ def get_transforms(need=('train', 'val'), img_size=(512, 384)):
         ], p=1.0)
     return transformations    
 
+def rand_bbox(size): # size : [B, C, H, W]
+    beta_arg = 1
+    W = size[3] # 이미지의 width
+    H = size[2] # 이미지의 height
+
+    w = W * np.random.beta(beta_arg, beta_arg)
+    h_minus = H * (1 - np.random.beta(beta_arg, beta_arg)) / 2
+    h_plus =  H * (1 + np.random.beta(beta_arg, beta_arg)) / 2
+
+    bbx1 = int(min(w,W/2))
+    bby1 = int(h_minus)
+    bbx2 = int(max(w,W/2))
+    bby2 = int(h_plus)
+    return bbx1, bby1, bbx2, bby2
+
+import math
+from torch.optim.lr_scheduler import _LRScheduler
+
+class CosineAnnealingWarmUpRestarts(_LRScheduler):
+    def __init__(self, optimizer, T_0, T_mult=1, eta_max=0.1, T_up=0, gamma=1., last_epoch=-1):
+        if T_0 <= 0 or not isinstance(T_0, int):
+            raise ValueError("Expected positive integer T_0, but got {}".format(T_0))
+        if T_mult < 1 or not isinstance(T_mult, int):
+            raise ValueError("Expected integer T_mult >= 1, but got {}".format(T_mult))
+        if T_up < 0 or not isinstance(T_up, int):
+            raise ValueError("Expected positive integer T_up, but got {}".format(T_up))
+        self.T_0 = T_0
+        self.T_mult = T_mult
+        self.base_eta_max = eta_max
+        self.eta_max = eta_max
+        self.T_up = T_up
+        self.T_i = T_0
+        self.gamma = gamma
+        self.cycle = 0
+        self.T_cur = last_epoch
+        super(CosineAnnealingWarmUpRestarts, self).__init__(optimizer, last_epoch)
+    
+    def get_lr(self):
+        if self.T_cur == -1:
+            return self.base_lrs
+        elif self.T_cur < self.T_up:
+            return [(self.eta_max - base_lr)*self.T_cur / self.T_up + base_lr for base_lr in self.base_lrs]
+        else:
+            return [base_lr + (self.eta_max - base_lr) * (1 + math.cos(math.pi * (self.T_cur-self.T_up) / (self.T_i - self.T_up))) / 2
+                    for base_lr in self.base_lrs]
+
+    def step(self, epoch=None):
+        if epoch is None:
+            epoch = self.last_epoch + 1
+            self.T_cur = self.T_cur + 1
+            if self.T_cur >= self.T_i:
+                self.cycle += 1
+                self.T_cur = self.T_cur - self.T_i
+                self.T_i = (self.T_i - self.T_up) * self.T_mult + self.T_up
+        else:
+            if epoch >= self.T_0:
+                if self.T_mult == 1:
+                    self.T_cur = epoch % self.T_0
+                    self.cycle = epoch // self.T_0
+                else:
+                    n = int(math.log((epoch / self.T_0 * (self.T_mult - 1) + 1), self.T_mult))
+                    self.cycle = n
+                    self.T_cur = epoch - self.T_0 * (self.T_mult ** n - 1) / (self.T_mult - 1)
+                    self.T_i = self.T_0 * self.T_mult ** (n)
+            else:
+                self.T_i = self.T_0
+                self.T_cur = epoch
+                
+        self.eta_max = self.base_eta_max * (self.gamma**self.cycle)
+        self.last_epoch = math.floor(epoch)
+        for param_group, lr in zip(self.optimizer.param_groups, self.get_lr()):
+            param_group['lr'] = lr
 
 def train(data_dir, model_dir, args):
     seed_everything(args.seed)
@@ -239,13 +295,25 @@ def train(data_dir, model_dir, args):
     model = select_model('efficientnet', 18).to(device)    
     # -- loss & metric
     criterion = create_criterion(args.criterion)  # default: cross_entropy
-    opt_module = getattr(import_module("torch.optim"), args.optimizer)  # default: SGD
-    optimizer = opt_module(
+    # opt_module = getattr(import_module("torch.optim"), args.optimizer)  # default: SGD
+    # optimizer = opt_module(
+    #     filter(lambda p: p.requires_grad, model.parameters()),
+    #     lr=args.lr,
+    #     weight_decay=5e-4
+    # )
+    optimizer = BERTAdam(
         filter(lambda p: p.requires_grad, model.parameters()),
-        lr=args.lr,
-        weight_decay=5e-4
+        lr=0,
+        weight_decay =0.5,
+        correct_bias=True
     )
-    scheduler = StepLR(optimizer, args.lr_decay_step, gamma=0.5)
+
+
+    #scheduler = StepLR(optimizer, args.lr_decay_step, gamma=0.5)
+    #print(len(train_loader))
+    #scheduler = OneCycleLR(optimizer, max_lr=1E-05, steps_per_epoch=len(train_loader), epochs=30)
+    scheduler = CosineAnnealingWarmUpRestarts(optimizer, T_0=1, T_mult=2, eta_max=5e-4, T_up=0, gamma=0.5)
+
 
     # -- logging
     logger = SummaryWriter(log_dir=save_dir)
@@ -255,7 +323,6 @@ def train(data_dir, model_dir, args):
     best_val_acc = 0
     best_val_loss = np.inf
     best_val_f1 = 0
-
     scaler = torch.cuda.amp.GradScaler()#####
     for epoch in tqdm(range(args.epochs), 'Training sequence'):
 
@@ -267,18 +334,64 @@ def train(data_dir, model_dir, args):
         list_labels = []
         list_preds = []
         train_set.dataset.set_transform(transform['train']) #
+
+        # from torch_lr_finder import LRFinder
+        # lr_finder = LRFinder(model, optimizer, criterion, device="cuda")
+        # lr_finder.range_test(train_loader, end_lr=100, num_iter=100)
+        # lr_finder.plot() # to inspect the loss-learning rate graph
+        # lr_finder.reset() # to reset the model and optimizer to their initial state
+
+
         for idx, train_batch in enumerate(train_loader):
             inputs, labels = train_batch
+            #fig , axes = plt.subplots(1,3)
+            #fig.set_size_inches(15,12)
+            # print("인풋임")
+            # print(inputs)
+            # print("라벨임")
+            # print(labels)
+            # print("인풋 모양")
+            # print(inputs.shape)
+            # mean=[0.485, 0.456, 0.406]
+            # std=[0.229, 0.224, 0.225]
+
+            # inv_normalize = transforms.Compose([
+            #     transforms.Normalize(mean = [ 0., 0., 0. ],std = [ 1/0.229, 1/0.224, 1/0.225 ]),
+            #     transforms.Normalize(mean = [ -0.485, -0.456, -0.406 ],std = [ 1., 1., 1. ])])
             
+
             inputs = inputs.to(device)
             labels = labels.to(device)
+            #print("#########################라벨입니다##########################")
+            #print(labels)
+            # -- cutmix
+            if np.random.random() >= 0: #cutmix 작동될 확률
+                rand_index = torch.randperm(inputs.size()[0]).to(device) # batch_size 내의 인덱스가 랜덤하게 셔플됩니다.
+                shuffled_labels = labels[rand_index] # 타겟 레이블을 랜덤하게 셔플합니다.
+                bbx1, bby1, bbx2, bby2 = rand_bbox(inputs.size())
+                #inputs[:,:,bbx1:bbx2, bby1:bby2] = inputs[rand_index,:,bbx1:bbx2, bby1:bby2]
+                inputs[:,:, bby1:bby2, bbx1:bbx2] = inputs[rand_index,:, bby1:bby2, bbx1:bbx2]
+                # plt.imshow(to_pil_image(inv_normalize(inputs[0])))
+                # plt.title(str(bbx1) + str(bby1)+ str(bbx2) +  str(bby2))
+                # print(bbx1, bby1, bbx2, bby2)
+                # plt.savefig('savefig_default.png')
+
+                lam = 1 - ((bbx2 - bbx1) * (bby2 - bby1) / (inputs.size()[-1] * inputs.size()[-2])) # 영역 사이즈가 달라짐
+                # print("#########################라벨입니다##########################")
+                # print(lam)
+
+                with torch.cuda.amp.autocast():#####
+                    outs = model(inputs)
+                    preds = torch.argmax(outs, dim=-1)
+                    loss = criterion(outs, labels) * lam + criterion(outs, shuffled_labels) * (1. - lam)
+            else:
+                with torch.cuda.amp.autocast():#####
+                    outs = model(inputs)
+                    preds = torch.argmax(outs, dim=-1)
+                    loss = criterion(outs, labels)
 
             optimizer.zero_grad()
 
-            with torch.cuda.amp.autocast():#####
-                outs = model(inputs)
-                preds = torch.argmax(outs, dim=-1)
-                loss = criterion(outs, labels)
             list_labels.append(labels.detach().cpu().numpy())
             list_preds.append(preds.detach().cpu().numpy())
             #list_preds.append(preds.cpu())	
@@ -287,9 +400,12 @@ def train(data_dir, model_dir, args):
             scaler.scale(loss).backward()
             #optimizer.step()
             scaler.step(optimizer)
-
             loss_value += loss.item()
+
+
             matches += (preds == labels).sum().item()
+
+            ##### 밑의 부분은 터미널로 출력하는 부분! 결과에 영향 없다 ###########
             if (idx + 1) % args.log_interval == 0:
                 train_loss = loss_value / args.log_interval
                 train_acc = matches / args.batch_size / args.log_interval
@@ -309,7 +425,9 @@ def train(data_dir, model_dir, args):
                 loss_value = 0
                 matches = 0
             scaler.update()
+            scheduler.step(epoch + idx / len(train_loader))
 
+        ### 이부분도 터미널 출력 부분 결과에 영향 XX
         tot_labels = np.concatenate(list_labels)
         tot_preds = np.concatenate(list_preds)
         train_f1 = f1_score(tot_labels, tot_preds, average='macro')
@@ -317,9 +435,7 @@ def train(data_dir, model_dir, args):
         print(f'tot_preds shape: {tot_preds.shape}, head: {tot_preds[:5]}, max: [{tot_preds.max()}], min: [{tot_labels.min()}]')
         print(f"Epoch[{epoch}/{args.epochs}] || training f1 {train_f1:.4}")                
 
-        scheduler.step()
 
-        #dataset.change_transform('val')############# val tranformation으로 바꿈
         # val loop
         with torch.no_grad():
             print("Calculating validation results...")
@@ -399,18 +515,18 @@ if __name__ == '__main__':
     load_dotenv(verbose=True)
 
     parser.add_argument('--seed', type=int, default=42, help='random seed (default: 42)')
-    parser.add_argument('--epochs', type=int, default=30, help='number of epochs to train (default: 1)')
+    parser.add_argument('--epochs', type=int, default=31, help='number of epochs to train (default: 1)')
     parser.add_argument('--dataset', type=str, default='MaskSplitByProfileDataset', help='dataset augmentation type (default: MaskBaseDataset)')
 #    parser.add_argument('--augmentation', type=str, default='BaseAugmentation', help='data augmentation type (default: BaseAugmentation)')
 #    parser.add_argument("--resize", nargs="+", type=list, default=[224, 224], help='resize size for image when training')
     parser.add_argument('--batch_size', type=int, default=32, help='input batch size for training (default: 64)')
     parser.add_argument('--valid_batch_size', type=int, default=32, help='input batch size for validing (default: 1000)')
 #     parser.add_argument('--model', type=str, default='BaseModel', help='model type (default: BaseModel)')
-    parser.add_argument('--optimizer', type=str, default='Adam', help='optimizer type (default: SGD)')
-    parser.add_argument('--lr', type=float, default=1e-4, help='learning rate (default: 1e-3)')
+#    parser.add_argument('--optimizer', type=str, default='Adam', help='optimizer type (default: SGD)')
+    parser.add_argument('--lr', type=float, default=1e-9, help='learning rate (default: 1e-3)')
     parser.add_argument('--val_ratio', type=float, default=0.2, help='ratio for validaton (default: 0.2)')
-    parser.add_argument('--criterion', type=str, default='focal', help='criterion type (default: cross_entropy)')
-    parser.add_argument('--lr_decay_step', type=int, default=10, help='learning rate scheduler decay step (default: 20)')
+    parser.add_argument('--criterion', type=str, default='label_smoothing', help='criterion type (default: cross_entropy)')
+#    parser.add_argument('--lr_decay_step', type=int, default=10, help='learning rate scheduler decay step (default: 20)')
     parser.add_argument('--log_interval', type=int, default=50, help='how many batches to wait before logging training status')
     parser.add_argument('--name', default='exp', help='model save at {SM_MODEL_DIR}/{name}')
 
